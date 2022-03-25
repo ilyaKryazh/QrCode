d72f351236b7990c42f36a0972956b6a3b1f5ab9
Ilya <ilya.kryazik10@gmail.com>
2022-03-25 11:04:02 +0200

    Error mesages

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FormValidator;
 use Illuminate\Http\Request;
 
 class CertificateController extends Controller
 {
-    public function index()
-    {
-        
-    }
 
-    public function store()
+    public function store(FormValidator $request)
     {
-
+        $validated = $request->validated();
     }
 
     public function download()
--- resources/views/certificateForm.blade.php
+++ resources/views/certificateForm.blade.php
@@ -4,16 +4,32 @@
     <div class="row justify-content-center">
         <form action="" method="post">
             <div class="mb-3">
-                <label for="formGroupExampleInput" class="form-label">Номер Сетификата</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Номер Сетификата">
+                <label for="formGroupExampleInput1" class="form-label">Номер Сетификата</label>
+                <input type="text" class="form-control" name="number" id="formGroupExampleInput1" placeholder="Номер Сетификата">
+                @error('number')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="formGroupExampleInput2" class="form-label">ФИО</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="ФИО">
+                <label for="formGroupExampleInput2" class="form-label">Название курса</label>
+                <input type="text" class="form-control" name="course" id="formGroupExampleInput2" placeholder="Название курса">
+                @error('course')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="formGroupExampleInput2" class="form-label">Дата завершения курса</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Дата завершения курса">
+                <label for="formGroupExampleInput3" class="form-label">ФИО</label>
+                <input type="text" class="form-control" name="name" id="formGroupExampleInput3" placeholder="ФИО">
+                @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="formGroupExampleInput4" class="form-label">Дата завершения курса</label>
+                <input type="date" class="form-control" name="date" id="formGroupExampleInput4" placeholder="Дата завершения курса">
+                @error('date')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="col-auto">
                 <button type="submit" class="btn btn-primary mb-3">Получить сертификат</button>
